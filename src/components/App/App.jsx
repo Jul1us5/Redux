0f2c0da317b1231c9cc087da538f2f6b37cc7863
“Julius” <julius.vlasovas@gmail.com>
2022-01-31 19:39:32 +0200

    Redux | submit form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,24 @@
-import React from "react";
-import style from './App.module.scss';
+import React, { useState } from "react";
+import style from "./App.module.scss";
 import PropTypes from "prop-types";
 import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const title = useSelector((state) => state.title);
+  const subTitle = useSelector((state) => state.subTitle);
 
-  const dispatch = useDispatch()
-  const title = useSelector((state) => state.title)
-  const subTitle = useSelector((state) => state.subTitle)
-  
+  const [formTitle, setFormTitle] = useState("");
+  const [formSubTitle, setFormSubTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formTitle) dispatch({ type: 'ADD_TITLE', payload: formTitle })
+    if (formSubTitle) dispatch({ type: 'ADD_SUB_TITLE', payload: formSubTitle })
+    
+    
+  };
 
   return (
     <div className={style.wrapper}>
@@ -16,6 +26,15 @@ const App = () => {
         <h1 className={style.title}>{title}</h1>
         <p className={style.p}>{subTitle}</p>
       </main>
+      <section className={style.section}>
+        <form onSubmit={handleSubmit}>
+          <label>Title:</label>
+          <input type="text" value={formTitle} onChange={(e) => setFormTitle(e.target.value)}/>
+          <label>Sub title:</label>
+          <input type="text" value={formSubTitle} onChange={(e) => setFormSubTitle(e.target.value)}/>
+          <button type="submit">submit</button>
+        </form>
+      </section>
     </div>
   );
 };
