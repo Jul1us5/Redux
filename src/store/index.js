af761d13ae5b67e9a627d8630bd34f57cb21b7bf
“Julius” <julius.vlasovas@gmail.com>
2022-02-02 13:36:26 +0200

    Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import { subTitleReducer } from './subTitleReducer.js';
 import { titleReducer } from './titleReducer.js';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
    title: titleReducer,
    subTitle: subTitleReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore( rootReducer, composeWithDevTools() )
